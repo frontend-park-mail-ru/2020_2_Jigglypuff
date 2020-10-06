2749094f7e353915d6fb1292b82732bf893bac27
Oladiy <bd3396121@gmail.com>
2020-10-06 18:28:41 +0300

    test files

---
+++ public/static/profiles.js
@@ -0,0 +1,20 @@
+profileList = [
+    {
+        name: "Томас",
+        surname: "Шелби",
+        login: "iamtommyshelby",
+        avatar: "../../assets/Tommys3.jpg"
+    },
+    {
+        name: "Энтони",
+        surname: "Старк",
+        login: "iamanthonystark",
+        avatar: "../../assets/Tommys3.jpg"
+    },
+    {
+        name: "Том",
+        surname: "Шел",
+        login: "iamtomshel",
+        avatar: "../../assets/Tommys3.jpg"
+    },
+]
