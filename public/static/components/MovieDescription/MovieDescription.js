58a6f73b09d42e774b66919b221a6a2a5e9b1526
Anthony Tintul <antontintul@gmail.com>
2020-10-06 13:55:51 +0300

    добавлена статика и шаблоны

---
+++ public/static/components/MovieDescription/MovieDescription.js
@@ -0,0 +1,17 @@
+export class MovieDescriptionComponent {
+    constructor({parentElement = document.body} = {}) {
+        this._el = parentElement;
+    }
+
+    set data(d) {
+        this._data = d;
+    }
+
+    get data() {
+        return this._data
+    }
+
+    render() {
+        this._el.appendChild(window.fest['static/components/MovieDescription/MovieDescription.tmpl'](this._data))
+    }
+}
---
+++ public/static/components/SignUp/SignUp.js
@@ -0,0 +1,17 @@
+export class SignUpComponent {
+    constructor({parentElement = document.body} = {}) {
+        this._el = parentElement;
+    }
+
+    set data(d) {
+        this._data = d;
+    }
+
+    get data() {
+        return this._data
+    }
+
+    render() {
+        this._el.appendChild(window.fest['static/components/SignUp/SignUp.tmpl'](this._data))
+    }
+}
