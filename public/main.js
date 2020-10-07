b0d39afe3903da0a10466312af33a7beb4a096ad
Oladiy <bd3396121@gmail.com>
2020-10-07 03:25:45 +0300

    Рабочая регистрация и авторизация. Валидация форм логина и пароля

--- public/main.js
+++ public/main.js
@@ -10,8 +10,6 @@ let movieContainer = new MovieContainerComponent({parentElement: container});
 let cinemaContainer = new CinemaContainerComponent({parentElement: container});
 let movieDescription = new MovieDescriptionComponent({parentElement: container});
 let profile = new ProfileComponent({parentElement: container});
-let signIn = new SignInComponent({parentElement: container});
-let signUp = new SignUpComponent({parentElement: container});
 
 movieContainer.render();
 
@@ -29,12 +27,14 @@ let moviesRef = document.getElementById('movies-navbar');
 let cinemaRef = document.getElementById('cinema-navbar');
 let profileRef = document.getElementById('profile-navbar');
 
+
 moviesRef.addEventListener('click', evt => {
     evt.preventDefault();
     movieContainer.data = moviesData;
     movieContainer.render()
     // container.innerHTML = window.fest['static/components/MovieContainer/MovieContainer.tmpl'](moviesData)
 })
+
 
 cinemaRef.addEventListener('click', evt => {
     evt.preventDefault();
@@ -46,62 +46,124 @@ cinemaRef.addEventListener('click', evt => {
 profileRef.addEventListener('click', evt => {
     evt.preventDefault();
 
-    let response = fetch('http://95.163.249.116:8080/getprofile/', {
+    fetch('http://95.163.249.116:8080/getprofile/', {
         method: 'GET',
-        credentials: "include",
+        credentials: "same-origin",
         headers: {
             "Origin": "http://localhost:63342",
-        }
+            "Cookie": "session_id=XVlBzgbaiCMRAjWwhTHctcuAxhxKQFDa"
+        },
     })
-        .then((res) => response = res)
-        .catch((res) => response = res);
-    alert(response);
-
-    //profile.data = profileList[1];
-    //profile.render();
-    signInPage();
+        .then(({status}) => {
+            if (status === 200) {
+                profile.data = profileList[1];
+                profile.render();
+            } else {
+                signInPage();
+            }
+        })
+        .catch(() => console.log("failed to get profile"));
 });
 
 
+function profilePage() {
+    profile.render();
+}
+
+function isLoginValid(login) {
+    return !!login.match(/^[0-9a-zA-Z]+$/);
+}
+
+function isPasswordValid(password) {
+    return !!password.match(/^[0-9a-zA-Z]+$/);
+}
+
 function signUpPage() {
+    const signUp = new SignUpComponent({parentElement: container});
     signUp.render();
-    let signUpForm = document.getElementById('signUpForm');
+    const signUpForm = document.forms["signUpForm"];
     signUpForm.addEventListener('submit', e => {
         e.preventDefault();
+
+        const login = signUpForm.elements.login.value.trim()
+        if (!isLoginValid(login)) {
+            alert("Invalid login or password!");
+            e.preventDefault();
+        }
+
+        const password = signUpForm.elements.password.value.trim()
+        if (!isPasswordValid(password)) {
+            alert("Invalid login or password!");
+            e.preventDefault();
+        }
+
         const response = fetch('http://95.163.249.116:8080/signup/', {
             method: 'POST',
-            body: '{"Login":"Bussat1223", "Password":"password"}',
+            body: '{"Login":"' + login + '", "Password":"' + password + '"}',
             credentials: "same-origin",
             headers: {
                 "Origin": "http://localhost:63342",
                 "Content-Type":"application/json",
             }
         });
+
+        response
+            .then(({status}) => {
+                if (status === 200) {
+                    alert("Success!");
+                    signInPage();
+                } else if (status === 401){
+                    alert("Something went wrong!");
+                }
+            })
+            .catch(({statusText}) => console.log(statusText));
     });
 }
 
 
 function signInPage() {
+    const signIn = new SignInComponent({parentElement: container});
     signIn.render();
-    let signInForm = document.getElementById('signInForm');
+    const signInForm = document.forms["signInForm"];
     signInForm.addEventListener('submit', e => {
         e.preventDefault();
+
+        const login = signInForm.elements.login.value.trim()
+        if (!isLoginValid(login)) {
+            alert("Invalid login or password!");
+            e.preventDefault();
+        }
+
+        const password = signInForm.elements.password.value.trim()
+        if (!isPasswordValid(password)) {
+            alert("Invalid login or password!");
+            e.preventDefault();
+        }
+
         const response = fetch('http://95.163.249.116:8080/signin/', {
             method: 'POST',
-            body: '{"Login":"Bussat1223", "Password":"password"}',
+            body: '{"Login":"' + login + '", "Password":"' + password + '"}',
             credentials: "same-origin",
             headers: {
                 "Origin": "http://localhost:63342",
                 "Content-Type":"application/json"
             },
         });
+        response
+            .then(({status}) => {
+                if (status === 200) {
+                    alert("Success!");
+                    profilePage();
+                } else if (status === 401) {
+                    alert("Something went wrong!");
+                }
+            })
+            .catch(({statusText}) => console.log(statusText));
     });
 
-    let signUp = document.getElementById('signUpPage');
+    const signUp = document.getElementById('signUpPage');
     signUp.addEventListener('click', e => {
         e.preventDefault();
         signUpPage();
     });
 }
-
-
