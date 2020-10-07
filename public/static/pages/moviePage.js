6d61aff467cf75eba69f934be8b32b92b6975557
Oladiy <bd3396121@gmail.com>
2020-10-07 15:38:05 +0300

    fix profile <a> v2

--- public/static/pages/moviePage.js
+++ public/static/pages/moviePage.js
@@ -14,62 +14,62 @@ export default function moviePage() {
             movieContainer.data = r;
             movieContainer.render();
             console.log("1");
-        });
-    }).then(() => {
-        let ref = container.getElementsByTagName("a");
-        Object.keys(ref).map((key) => {
-            ref[key].addEventListener('click', evt => {
-                evt.preventDefault();
-                console.log("2");
+            let ref = container.getElementsByTagName("a");
+            Object.keys(ref).map((key) => {
+                ref[key].addEventListener('click', evt => {
+                    evt.preventDefault();
+                    console.log("2");
 
-                movieDescription.data = movieContainer.data[key];
-                const ratingResponse = fetch('http://cinemascope.space/getmovierating/?name=' + movieDescription.data.Name);
-                ratingResponse.then((rateRes) => {
-                    console.log("2.1");
-                    if (rateRes.status === 200) {
-                        rateRes.json().then(rating => {
-                            movieDescription.data.Rating = rating;
-                        })
-                    } else {
-                        movieDescription.data.Rating = 0;
-                    }
-                });
-                movieDescription.render();
-            });
-        });
-    }).then(() => {
-        const rateButton = document.getElementById('rate');
-        const rate = document.getElementsByTagName('select')[0];
-
-        rateButton.addEventListener('click', event => {
-            event.preventDefault();
-            console.log("3");
-            const rateResponse = fetch('http://cinemascope.space/ratemovie/', {
-                method: 'POST',
-                body: '{"Name":"' + movieDescription.data.Name + '", "Rating":' + rate.value + '}',
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            });
-            rateResponse.then((rateRes) => {
-                if (rateRes.status !== 401) {
+                    movieDescription.data = movieContainer.data[key];
                     const ratingResponse = fetch('http://cinemascope.space/getmovierating/?name=' + movieDescription.data.Name);
-                    ratingResponse.then(successRatingResponse => {
-                        successRatingResponse.json().then(r => {
-                            console.log("5");
-                            movieContainer.data.Rating = r;
-                            movieContainer.render();
+                    ratingResponse.then((rateRes) => {
+                        console.log("2.1");
+                        if (rateRes.status !== 401) {
+                            console.log("2.3")
+                            rateRes.json().then(rating => {
+                                movieDescription.data.Rating = rating;
+                            })
+                        } else {
+                            movieDescription.data.Rating = 0;
+                        }
+                    });
+                    movieDescription.render();
+
+                    const rateButton = document.getElementById('rate');
+                    const rate = document.getElementsByTagName('select')[0];
+
+                    rateButton.addEventListener('click', event => {
+                        event.preventDefault();
+                        console.log("3");
+                        const rateResponse = fetch('http://cinemascope.space/ratemovie/', {
+                            method: 'POST',
+                            body: '{"Name":"' + movieDescription.data.Name + '", "Rating":' + rate.value + '}',
+                            credentials: "include",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                        });
+                        rateResponse.then((rateRes) => {
+                            if (rateRes.status !== 401) {
+                                const ratingResponse = fetch('http://cinemascope.space/getmovierating/?name=' + movieDescription.data.Name);
+                                ratingResponse.then(successRatingResponse => {
+                                    successRatingResponse.json().then(r => {
+                                        console.log("5");
+                                        movieDescription.data.Rating = r;
+                                        movieDescription.render();
+                                    });
+                                });
+                            } else {
+                                movieDescription.render();
+                                let rateBtn = document.getElementById("rate");
+                                const errorMessage = document.createElement('p');
+                                errorMessage.style.cssText = "border: 3px solid red; border-radius: 10px;"
+                                errorMessage.innerHTML = "Please sign in";
+                                rateBtn.outerHTML = errorMessage.outerHTML;
+                            }
                         });
                     });
-                } else {
-                    movieDescription.render();
-                    let rateBtn = document.getElementById("rate");
-                    const errorMessage = document.createElement('p');
-                    errorMessage.style.cssText = "border: 3px solid red; border-radius: 10px;"
-                    errorMessage.innerHTML = "Please sign in";
-                    rateBtn.outerHTML = errorMessage.outerHTML;
-                }
+                });
             });
         });
     }).catch((res) => alert(res.statusCode));
