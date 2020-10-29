40f3c8180e2994d86b0b01679dc4e79428c479d5
Oladiy <bd3396121@gmail.com>
2020-10-30 00:57:24 +0300

    [ADD] Extractor for movie data

--- src/utils/Extractor.js
+++ src/utils/Extractor.js
@@ -8,29 +8,92 @@ export default class Extractor {
     static extractFormData(data) {
         const result = new Map();
 
-        if (data['state'].login !== '') {
+        if (!data['state'].login.length) {
             result['login'] = data['state'].login;
         }
 
-        if (data['state'].password !== '') {
+        if (!data['state'].password.length) {
             result['password'] = data['state'].password;
         }
 
-        if (data['state'].email !== '') {
+        if (!data['state'].email.length) {
             result['email'] = data['state'].email;
         }
 
-        if (data['state'].name !== '') {
+        if (!data['state'].name.length) {
             result['name'] = data['state'].name;
         }
 
-        if (data['state'].surname !== '') {
+        if (!data['state'].surname.length) {
             result['surname'] = data['state'].surname;
         }
 
 
-        if (data['state'].avatar !== '') {
+        if (!data['state'].avatar.length) {
             result['avatar'] = data['state'].avatar;
+        }
+
+        return result;
+    }
+
+    /**
+     * Extract filled fields from movieData form.
+     * @param {MovieModel} data
+     * @return {Map}
+     */
+    static extractMovieData(data) {
+        const result = new Map();
+
+        if (!data.ageLimit.length) {
+            result['ageLimit'] = data.ageLimit;
+        }
+
+        if (!data.country.length) {
+            result['country'] = data.country;
+        }
+
+        if (!data.cover.length) {
+            result['cover'] = data.cover;
+        }
+
+        if (!data.description.length) {
+            result['description'] = data.description;
+        }
+
+        if (!data.director.length) {
+            result['director'] = data.director;
+        }
+
+        if (!data.duration.length) {
+            result['duration'] = data.duration;
+        }
+
+        if (!data.genre.length) {
+            result['genre'] = data.genre;
+        }
+
+        if (!data.name.length) {
+            result['name'] = data.name;
+        }
+
+        if (!data.ratingGlobal.length) {
+            result['ratingGlobal'] = data.ratingGlobal;
+        }
+
+        if (!data.ratingUser.length) {
+            result['ratingUser'] = data.ratingUser;
+        }
+
+        if (!data.reviews.length) {
+            result['reviews'] = data.reviews;
+        }
+
+        if (!data.starring.length) {
+            result['starring'] = data.starring;
+        }
+
+        if (!data.year.length) {
+            result['year'] = data.year;
         }
 
         return result;
