13eb66a14d7caef7939201df3d1569379b9e236c
Oladiy <bd3396121@gmail.com>
2020-11-04 22:50:28 +0300

    [UPDATE] State type to map.

--- src/viewmodels/SignInViewModel.js
+++ src/viewmodels/SignInViewModel.js
@@ -6,10 +6,10 @@ export default class SignInViewModel {
      * Represents SignIn ViewModel constructor
      */
     constructor() {
-        this.state = {
-            login: '',
-            password: '',
-        };
+        this.state = new Map([
+            ['login', ''],
+            ['password', ''],
+        ]);
         this.signInCommand = {exec: () => this.signIn()};
     }
 
@@ -21,20 +21,17 @@ export default class SignInViewModel {
         const userModel = new UserModel();
 
         userModel.login = this.state.login;
-        if (!userModel.login) {
-            throw Error('invalid login or password');
-        }
-
         userModel.password = this.state.password;
-        if (!userModel.password) {
+
+        if (!userModel.login || !userModel.password) {
             throw Error('invalid login or password');
         }
 
         const response = await userModel.signIn();
         if (response.ok) {
             return response.ok;
-        } else {
-            throw new Error('invalid login or password');
         }
+
+        throw new Error('invalid login or password');
     }
 }
