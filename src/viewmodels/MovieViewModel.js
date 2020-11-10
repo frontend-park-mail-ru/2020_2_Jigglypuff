ea50d1635a8438f932b2b8a5fdc2862f4aa5d785
Oladiy <bd3396121@gmail.com>
2020-11-10 16:42:06 +0300

    [FIX] Time extraction from premierTime.

--- src/viewmodels/MovieViewModel.js
+++ src/viewmodels/MovieViewModel.js
@@ -57,7 +57,7 @@ export default class MovieViewModel extends BaseViewModel {
             this.schedule[this.schedule.length - 1][key] = value;
         });
 
-        this.schedule[this.schedule.length - 1]['time'] = this.schedule[this.schedule.length - 1]['time'].replace(/\d{4}-\d{2}-\d{2}T/, '');
+        this.schedule[this.schedule.length - 1]['time'] = this.schedule[this.schedule.length - 1]['premierTime'].replace(/\d{4}-\d{2}-\d{2}T/, '');
         this.schedule[this.schedule.length - 1]['time'] = this.schedule[this.schedule.length - 1]['time']
             .replace(this.schedule[this.schedule.length - 1]['time'].replace(/\d{2}:\d{2}/, ''), '');
     }
