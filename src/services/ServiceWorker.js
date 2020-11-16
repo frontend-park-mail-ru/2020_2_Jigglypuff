d20deea518980ef69248b08415ed750b60078bb6
Oladiy <bd3396121@gmail.com>
2020-11-17 00:38:37 +0300

    [REMOVED] index.js from sw

--- src/services/ServiceWorker.js
+++ src/services/ServiceWorker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'CinemaScope_serviceWorker_2020_2-v1';
 const cacheUrls = [
     '/',
     '/index.html',
-    '/static/index.js',
+    //'/static/index.js',
     'css/index.css',
     'css/vars.css',
     /*'img/button/playButton.svg',
