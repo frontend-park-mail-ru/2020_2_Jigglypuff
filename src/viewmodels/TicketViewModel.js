8678b9d2156b6292923e7fa09d2db78542892648
Oladiy <bd3396121@gmail.com>
2020-12-20 18:35:06 +0300

    [ADD] Purchase by ETH

--- src/viewmodels/TicketViewModel.js
+++ src/viewmodels/TicketViewModel.js
@@ -1,5 +1,6 @@
 import Errors from 'consts/Errors';
 import TicketModel from 'models/TicketModel';
+import ETHManager from 'utils/ETH';
 import Extractor from 'utils/Extractor';
 
 /** Class that contains Ticket ViewModel */
@@ -27,7 +28,13 @@ export default class TicketViewModel {
             },
             scheduleID: '',
         };
+        this.stateTransaction = {
+            senderAddress: '',
+            signedTransaction: '',
+            transactionHash: '',
+        };
         this.buyTicketCommand = {exec: () => this.buyTicket()};
+        this.buyTicketByCryptoCommand = {exec: () => this.buyTicketByCrypto()};
         this.getTicketcommand = {exec: () => this.getTicket()};
     }
 
@@ -35,14 +42,24 @@ export default class TicketViewModel {
      * Buy ticket.
      * @return {Promise<JSON>}
      */
-    async buyTicket() {
+    async buyTicket(transactionHash = '') {
         const ticketModel = new TicketModel();
 
         ticketModel.login = this.state.login;
         ticketModel.placeFields = this.state.placeFields;
         ticketModel.scheduleID = Number(this.state.scheduleID);
 
-        return ticketModel.buyTicket();
+        if (transactionHash.length > 0) {
+            return ticketModel.buyTicket(transactionHash,
+                this.stateTransaction.senderAddress,
+                this.stateTransaction.signedTransaction,
+            );
+        }
+
+        return ticketModel.buyTicket(this.stateTransaction.transactionHash,
+                                     this.stateTransaction.senderAddress,
+                                     this.stateTransaction.signedTransaction,
+                                    );
     }
 
     /**
@@ -65,5 +82,28 @@ export default class TicketViewModel {
         }
 
         throw new Error(Errors.FailedToGetTicket);
+    }
+
+    /**
+     * Buy ticket by ETH
+     * @return {Promise<Error>|Promise<Object>}
+     */
+    async buyTicketByCrypto() {
+        const cryptoManager = new ETHManager();
+        const response = await cryptoManager.transferSignedTransaction(this.stateTransaction.senderAddress,
+                                                                       this.stateTransaction.signedTransaction);
+        switch (response) {
+            case Errors.TransactionNonceIsAlreadyUsed:
+                return Errors.TransactionNonceIsAlreadyUsed;
+            case Errors.TransactionNotEnoughMoney:
+                return Errors.TransactionNotEnoughMoney;
+
+            case Errors.TransactionVerificationIsFailed:
+                return Errors.TransactionVerificationIsFailed;
+            default:
+                break;
+        }
+
+        return this.buyTicket(response);
     }
 }
